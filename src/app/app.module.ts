40320c38f2dd6f585127f15625d086b2023179c6
prerakpratap <prerakpratap@gmail.com>
2018-07-27 11:50:02 +0530

    added-animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-
+import { FontAwesomeModule } from '@fortawesome/fontawesome-free';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { QueryformComponent } from './queryform/queryform.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 const routes : Routes =[
   {path : ' ' ,component : HeaderComponent},
   {path : 'admin-dashboard' ,component : AdminDashboardComponent},
@@ -21,10 +23,15 @@ const routes : Routes =[
     AppComponent,
     HeaderComponent,
     AdminDashboardComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    QueryformComponent,
+    
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFontAwesomeModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+firstslider="apartments-architectural-design-architecture-534233.jpg";
+secondslider="architecture-building-business-284257.jpg";
+thirdslider="apartment-architectural-design-architecture-379940.jpg";
   constructor() { }
 
   ngOnInit() {
