913f6484d0eebbea03ffd9a82d7c0642bba22db5
kunaldawra9184 <kunal.dawra1994@gmail.com>
2018-08-08 14:31:08 +0530

    updated event components and added society JSON file for event component

--- src/app/events/common-form/common-form.component.ts
+++ src/app/events/common-form/common-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Http} from '@angular/http'
 
 
 
@@ -10,17 +10,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommonFormComponent implements OnInit {
 
- 
+  model={
+
+    commonevents:'',
+    datefrom:'',
+    dateto:'',
+    timestart:'',
+    timeend:''
+  }
+
   //Select event type
-  commonEvents=["Holi","Diwali","Durga Puja","Dussehra","Christmas","New Year","Independence Day","Republic Day","Janmashtmi","Eid","Baisakhi","Lohri","Makar Sankaranti"];
+ commonEvents=[
+  {
+    id:1,
+    name:"Holi"
+  },
+  {
+    id:2,
+    name:"Diwali"
+  },
+  {
+    id:3,
+    name:"Durga Puja"
+  },
+  {
+    id:4,
+    name:"Dussehra"
+  },
+  {
+    id:5,
+    name:"Christmas"
+  },
+  {
+    id:6,
+    name:"New Year"
+  },
+  {
+    id:7,
+    name:"Independence Day"
+  },
+  {
+    id:8,
+    name:"Republic Day"
+  },
+  {
+    id:9,
+    name:"Janmashtmi"
+  },
+  {
+    id:10,
+    name:"Eid"
+  },
+  {
+    id:11,
+    name:"Baisakhi"
+  },
+  {
+    id:12,
+    name:"Lohri"
+  },
+  {
+    id:13,
+    name:"Makar Sankaranti"
+  }
+];
 
-  //Select society members
-  commonMembers=["Ajay Srivastava","Shankar Mahanjan","Sanjay Dutt","Sunil Kumar","Angad Negi"];
+  // //Select society members
+  // commonMembers=["Ajay Srivastava","Shankar Mahanjan","Sanjay Dutt","Sunil Kumar","Angad Negi"];
 
-  //Select hall type
-  commonHalls=["Hall 1, Block A","Hall 2, Block B","Hall 3, Block C","Hall 4, Block D"]
-   
+  // //Select hall type
+  // commonHalls=["Hall 1, Block A","Hall 2, Block B","Hall 3, Block C","Hall 4, Block D"]
   
+  constructor(private http:Http){
+
+  }
+
+  eventobj:object={};
+
+  addCommonEvent= function(event){
+    this.eventobj={
+      "commonEvents":event.commonEvents,
+      "dateFrom":event.dateFrom,
+      "dateTo":event.dateTo,
+      "timeStart":event.timeStart,
+      "timeEnd":event.timeEnd
+    }
+    this.http.post("http://localhost:3000/commonevents", this.eventobj).subscribe(
+          (res:Response)=>{
+            console.log(res);
+            // this.isAdded=true;
+          }
+        )
+  }
+
   ngOnInit() {
   }
 
--- src/app/events/owner-event-form/owner-event-form.component.ts
+++ src/app/events/owner-event-form/owner-event-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers} from '@angular/http'
+import  {NgForm} from '@angular/forms';
+import {EventType} from '../event-shared/eventType.model';
+ 
 
 @Component({
   selector: 'app-owner-event-form',
@@ -6,24 +10,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-event-form.component.css']
 })
 export class OwnerEventFormComponent implements OnInit {
+  
+  model={
+
+    name:'',
+    personalevents:'',
+    datefrom:'',
+    dateto:'',
+    timestart:'',
+    timeend:''
+  }
 
       //Select event type
-      personalEvents=["Birthday Party","Anniversary Party","Family Get Together","House Warming","Kitty Party","Family Funciton","Baby Shower"];
+      personalEvents=[
+        {
+          id:1,
+          name:"Birthday Party"
+        },
+        {
+          id:2,
+          name:"Anniversary Party"
+        },
+        {
+          id:3,
+          name:"Family Get Together"
+        },
+        {
+          id:4,
+          name:"House Warming"
+        },
+        {
+          id:5,
+          name:"Kitty Party"
+        },
+        {
+          id:6,
+          name:"Family Funciton"
+        },
+        {
+          id:7,
+          name:"Baby Shower"
+        }
+      ];
 
       //Select society members
-      commonMembers=["Ajay Srivastava","Shankar Mahanjan","Sanjay Dutt","Sunil Kumar","Angad Negi"];
+      // commonMembers=["Ajay Srivastava","Shankar Mahanjan","Sanjay Dutt","Sunil Kumar","Angad Negi"];
     
       //Select hall type
-      commonHalls=["Hall 1, Block A","Hall 2, Block B","Hall 3, Block C","Hall 4, Block D"]
+      // commonHalls=["Hall 1, Block A","Hall 2, Block B","Hall 3, Block C","Hall 4, Block D"]
 
-      model={
+      // personaleventsHasError=true;
 
-        name:'',
-        phone:'',
-        email:''
+      constructor(private http:Http){}
+
+      eventobj:object={};
+
+      addNewEvent= function(event){
+        this.eventobj={
+          "name":event.name,
+          "personalEvents":event.personalEvents,
+          "dateFrom":event.dateFrom,
+          "dateTo":event.dateTo,
+          "timeStart":event.timeStart,
+          "timeEnd":event.timeEnd
+        }
+        this.http.post("http://localhost:3000/events", this.eventobj).subscribe(
+          (res:Response)=>{
+            console.log(res);
+            // this.isAdded=true;
+          }
+        )
       }
 
   ngOnInit() {
+    
   }
 
 }
