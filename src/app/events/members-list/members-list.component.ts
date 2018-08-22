baa642f4d30974b0aa6a5763820dfa45a6f9dfc1
manipannu20 <harpreet.kaur@greatwits.com>
2018-08-22 12:50:23 +0530

    member list

--- src/app/events/members-list/members-list.component.ts
+++ src/app/events/members-list/members-list.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-members-list',
   templateUrl: './members-list.component.html',
   styleUrls: ['./members-list.component.css']
 })
-export class MembersListComponent implements OnInit {
+export class MembersListComponent{
+flatno:any;
+selectedAll:any;
+constructor(){
+  this.societyMembers
+}
 
-  constructor() { }
+_form={
+  key:0,
+  name:''
+}
 
-  ngOnInit() {
+societyMembers=[
+  {name:'Harpreet',flatno:'BlockA101', selected:false},
+  {name:'Kartik',flatno:'BlockA102',selected:false},
+  {name:'Shanu',flatno:'BlockA103',selected:false},
+  {name:'Kunal',flatno:'BlockA104',selected:false},
+  {name:'Pragati',flatno:'BlockA105',selected:false},
+  {name:'Dheeraj',flatno:'BlockB101',selected:false},
+  {name:'Nikhil',flatno:'BlockB102',selected:false},
+  {name:'Jyoti',flatno:'BlockB103',selected:false},
+  {name:'Ritu',flatno:'BlockB104',selected:false},
+  {name:'Ritu',flatno:'BlockB105',selected:false},
+
+]
+
+selectAll(){
+  for(var i=0;i<this.societyMembers.length;i++){
+    this.societyMembers[i].selected = this.selectedAll;
   }
-
+}
+  checkedIfAllSelected(){
+    this.selectedAll=this.societyMembers.every(function(item:any){
+      return item.selected == true;
+    })
+  }
 }
